90f3538208c0655c73b709273c8208a38a775af0
Jaipall <singhjaipal210503@gmail.com>
2025-06-16 13:46:14 +0530

    day1 prac

---
+++ day1/app.js
@@ -0,0 +1,33 @@
+// window.console.log("Hello from JS!");
+
+// let, char ,var 
+
+// variable have defult value is undefine
+// console.log(block);
+// console.log(age);
+// var age=24;
+// accces before intialization, re-declaration, re-assignment
+ 
+// console.log(age);
+
+// var age=23;
+// console.log(age);
+
+//-----------------------------------------------
+
+// console.log(age);// temperal dead zone
+// let age=24;     //declaration && initialization
+// console.log(age);
+// // let age=25;
+// age=23;// re-assignment is allow
+// console.log(age);
+
+//--------------------------------------
+
+const age=34;
+console.log(age);
+
+//-------------------------------
+// 1+1;
+
+//type coersion implict
